a5d0261e91e640b759c5b5f74eb3ffe0b45125cd
StefanyGuevara <stefany.m.guevara00@gmail.com>
2022-03-30 12:02:34 -0600

    Add project files.

---
+++ MySQLFun/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using MySQLFun.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+        private IBowlerRepository _repo { get; set; }
+
+        //Constructor
+        public HomeController(IBowlerRepository temp)
+        {
+            _repo = temp;
+        }
+
+        public IActionResult Index()
+        {
+            //List<Teams> teams = _repo.Teams.ToList();
+
+            var blah = _repo.Bowlers
+                //.OrderBy(x => x.)
+                .ToList();
+
+            return View(blah);
+        
+        }
+    }
+}
---
+++ MySQLFun/Models/EFBowlersRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySQLFun.Models
+{
+    public class EFBowlersRepository : IBowlerRepository
+    {
+        private BowlersDbContext _context { get; set; }
+        public EFBowlersRepository (BowlersDbContext temp)
+        {
+            _context = temp;
+        }
+
+        public IQueryable<Bowler> Bowlers => _context.Bowlers;
+    }
+}
